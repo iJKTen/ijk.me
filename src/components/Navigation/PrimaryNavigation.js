98dacecfb79e214c0b509403d6d0a344fd290a6b
ijk <ijkten@no-email.com>
2020-04-26 22:46:07 -0400

    using Link from @reach/router in navigation and this does not make another server request

--- src/components/Navigation/PrimaryNavigation.js
+++ src/components/Navigation/PrimaryNavigation.js
@@ -3,6 +3,7 @@ import { Nav, UL } from './PrimaryNavigationStyle';
 import NavToggle from './NavToggle';
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import { Link } from '@reach/router';
 
 const PrimaryNavigation = (props) => {
 
@@ -16,11 +17,28 @@ const PrimaryNavigation = (props) => {
       <UL
         css={{
           display: toggleState.open ? 'block' : 'none'
-        }}
-      >
-        <li><a href="/">Home</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/resume">Resume</a></li>
+        }}>
+        <li>
+          <Link 
+            to="/" 
+            onClick={() => setToggleState({open: false})}>
+              Home
+          </Link>
+        </li>
+        <li>
+          <Link 
+            to="projects"
+            onClick={() => setToggleState({open: false})}>
+              Projects
+          </Link>
+        </li>
+        <li>
+          <Link 
+            to="resume"
+            onClick={() => setToggleState({open: false})}>
+              Resume
+          </Link>
+        </li>
         <li><a href="//ijk.ghost.io" target="_blank" rel="noopener noreferrer nofollow">Blog</a></li>
         <li><a href="//github.com/ijkten" target="_blank" rel="noopener noreferrer nofollow">GitHub</a></li>
       </UL>
