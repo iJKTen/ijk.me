b2d08a117fc22a618e56d89c0591094955a5ad69
ijk <ijkten@no-email.com>
2020-06-13 19:33:52 -0400

    lazy load resume

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { GlobalStyle } from './components/GlobalStyled';
 import { Router } from '@reach/router';
 import PrimaryNavigation from './components/Navigation/PrimaryNavigation';
 import Home from './screens/Home/Home';
 import Footer from './components/Footer/Footer';
-import Resume from './screens/Resume/Resume';
+const Resume = lazy(() => import('./screens/Resume/Resume'));
 
 function App() {
   return (
