f8afb1e5d56af8e1f99978a019f1bca595f93fe5
ijk <ijkten@no-email.com>
2020-06-13 19:40:44 -0400

    removed lazy loading of resume until i figure out how to use it properly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import { GlobalStyle } from './components/GlobalStyled';
 import { Router } from '@reach/router';
 import PrimaryNavigation from './components/Navigation/PrimaryNavigation';
 import Home from './screens/Home/Home';
 import Footer from './components/Footer/Footer';
-const Resume = lazy(() => import('./screens/Resume/Resume'));
+import Resume from './screens/Resume/Resume';
 
 function App() {
   return (
